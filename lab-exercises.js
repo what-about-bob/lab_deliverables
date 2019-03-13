ec4ac4018ddf8c9e340b4c5d95e13d289e13eb81
Parick Cowan <patrick.w.cowan@gmail.com>
2019-03-13 08:38:29 -0500

    added patrick

---
+++ lab-exercises.js
@@ -0,0 +1,22 @@
+///Patrick's Lab Deliverable
+
+// Exercise 1
+// The parameter weekday is true if it is a weekday, and the parameter vacation
+// is true if we are on vacation. We sleep in if it is not a weekday or we're on ' +
+// 'vacation. Return true if we sleep in.
+//
+// Examples
+//
+// sleepIn(true, true) → true
+// sleepIn(true, false) → false
+// sleepIn(false, true) → true
+
+// function sleepIn(weekday,vacation) {
+//     if (vacation === true || weekday === false) {
+//         return true
+//     } else if (weekday === true){
+//         return false
+//     }
+// }
+//
+// console.log(sleepIn());
